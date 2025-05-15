f02601e78feacc3b9c402a6b0aa334bb25a205a8
aspectie <kovleshenko99@mail.ru>
2025-05-15 09:59:45 +0300

    release

--- dist/echarts.common.js
+++ dist/echarts.common.js
@@ -16981,6 +16981,24 @@
         var ticks = this.getTicks(true, true);
         var minorTicks = [];
         var extent = this.getExtent();
+        var intervalStats = {};
+        var dominantInterval = null;
+        var maxCount = 0;
+
+        for (var i = 1; i < ticks.length; i++) {
+          var interval = ticks[i].value - ticks[i - 1].value;
+          var count = (intervalStats[interval] || 0) + 1;
+          intervalStats[interval] = count;
+
+          if (count > maxCount && interval > 0) {
+            maxCount = count;
+            dominantInterval = interval;
+          }
+        }
+
+        var dominantSplits = this.getMinorSplits(dominantInterval);
+        var dominantSplitNumber = dominantSplits > 10 ? Math.ceil(dominantSplits / 2) : dominantSplits;
+        var dominantMinorInterval = dominantInterval / dominantSplitNumber;
 
         for (var i = 1; i < ticks.length; i++) {
           var nextTick = ticks[i];
@@ -16988,19 +17006,10 @@
           var count = 0;
           var minorTicksGroup = [];
           var interval = nextTick.value - prevTick.value;
-          var minorSplits = this.getMinorSplits(interval);
-          var splitNumber = minorSplits > 10 ? Math.ceil(minorSplits / 2) : minorSplits; // reduce too many splits
-
-          var minorInterval = interval / splitNumber;
-          var unit = getUnitByInterval(this._interval);
-          var currentUnit = getUnitByInterval(interval);
-
-          if (getPrimaryTimeUnit(currentUnit) !== getPrimaryTimeUnit(unit)) {
-            continue;
-          }
+          var splitNumber = Math.round(interval / dominantMinorInterval);
 
           while (count < splitNumber - 1) {
-            var minorTick = roundNumber$1(prevTick.value + (count + 1) * minorInterval); // For the first and last interval. The count may be less than splitNumber.
+            var minorTick = roundNumber$1(prevTick.value + (count + 1) * dominantMinorInterval); // For the first and last interval. The count may be less than splitNumber.
 
             if (minorTick > extent[0] && minorTick < extent[1]) {
               minorTicksGroup.push(minorTick);
